da6f587a2ab6c2dcbdfff674a26631f93d2c284b
BmmNdr <abiemmibi@gmail.com>
2024-05-24 10:52:22 +0200

    Better Styling

--- src/DashTest.py
+++ src/DashTest.py
@@ -2,31 +2,29 @@ import dash
 from dash import dcc, html
 import plotly.express as px
 import pandas as pd
-
+import dash_bootstrap_components as dbc
 import lastRaceDriverFastLap as lrfl
 
-# Initialize the app
-app = dash.Dash(__name__)
+# Initialize the app - incorporate a Dash Bootstrap theme
+external_stylesheets = [dbc.themes.CERULEAN]
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 fig = lrfl.get_fastest_laps_fig()
 
-app.layout = html.Div(children=[
-    html.Div([
-        html.H1("Last Race Drivers Best Lap", style={'text-align': 'center'}),
-        dcc.Graph(id='bar-graph', figure=fig),
-    ], style={'width': '50%', 'display': 'inline-block'}),
+app.layout = dbc.Container([
+    dbc.Row([
+        html.H1('F1 Dashboard', className="text-primary text-center fs-3")
+    ]),
+    
+    
+    dbc.Row([
+        #Last Race Best Laps Section
+        dbc.Col([
+            html.H4("Last Race Drivers Best Lap", style={'text-align': 'center', 'margin': 'auto', 'padding': '10px', 'color': 'Black'}),
+            dcc.Graph(id='bar-graph', figure=fig),
+        ], style={'width': 'fit-content', 'display': 'inline-block', 'border': '1px solid black', 'overflow': 'auto'}),
+    ]),
 ])
-
-# Define callback to update graph based on dropdown selection
-#@app.callback(
-#    dash.dependencies.Output('bar-graph', 'figure'),
-#    [dash.dependencies.Input('city-dropdown', 'value')]
-#)
-#
-#def update_graph(selected_city):
-#    filtered_df = df[df['City'] == selected_city]
-#    fig = px.bar(filtered_df, x='Fruit', y='Amount', color='City', barmode='group')
-#    return fig
 
 
 if __name__ == '__main__':
